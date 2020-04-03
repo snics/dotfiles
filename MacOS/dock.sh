af3f8b74e458c8051e40f322d64a326b1a4706c2
Nico Swiatecki <nico@devninjas.io>
2020-04-03 22:38:15 +0200

    Add MacOS defaults

---
+++ MacOS/dock.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Dock, Dashboard, and hot corners                                            #
+###############################################################################
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Enable spring loading for all Dock items
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don’t use
+# the Dock to launch apps.
+defaults write com.apple.dock persistent-apps -array
+
+# Show only open applications in the Dock
+#defaults write com.apple.dock static-only -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Don’t group windows by application in Mission Control
+# (i.e. use the old Exposé behavior instead)
+defaults write com.apple.dock expose-group-by-app -bool false
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Automatically hide and show the Dock
+#defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Don’t show recent applications in Dock
+#defaults write com.apple.dock show-recents -bool false
+
+# Disable the Launchpad gesture (pinch with thumb and three fingers)
+#defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+
+# Reset Launchpad, but keep the desktop wallpaper intact
+find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
+
+# Add iOS & Watch Simulator to Launchpad
+#sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
+#sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (Watch).app" "/Applications/Simulator (Watch).app"
+
+# Add a spacer to the left side of the Dock (where the applications are)
+#defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+# Add a spacer to the right side of the Dock (where the Trash is)
+#defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# 13: Lock Screen
+# Top left screen corner → Mission Control
+#defaults write com.apple.dock wvous-tl-corner -int 12
+#defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right screen corner → Desktop
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right screen corner → Start screen saver
+defaults write com.apple.dock wvous-b2-corner -int 2
+defaults write com.apple.dock wvous-b2-modifier -int 0
+
+
+###############################################################################
+# Add Apps to Dock                                                            #
+###############################################################################
+
+
+# Warning:
+# Double quotes Whenever you use $HOME in the path
+# Single quotes for the rest of the applications
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add '/Applications/System Preferences.app'
+dockutil --no-restart --add '/Applications/Spotify.app'
+
+# Add space to System configuration
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
+
+dockutil --no-restart --add '/Applications/Safari.app'
+dockutil --no-restart --add '/Applications/Google Chrome.app'
+dockutil --no-restart --add '/Applications/Firefox.app'
+dockutil --no-restart --add '/Applications/Opera.app'
+
+# Add space to System configuration
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
+
+dockutil --no-restart --add "/Applications/Spark.app"
+dockutil --no-restart --add "/Applications/Fantastical.app"
+dockutil --no-restart --add "/Applications/Cardhop.app"
+dockutil --no-restart --add "/Applications/Todoist.app"
+dockutil --no-restart --add "/Applications/Franz.app"
+
+# Add space to System configuration
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
+
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/GitKraken.app"
+dockutil --no-restart --add "/Applications/WebStorm.app"
+
+# Add space to System configuration
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
+
+dockutil --add '~/Downloads' --view list --display folder --allhomes
+dockutil --add '/Applications' --view grid --display folder --allhomes
--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 **Warning:** If you want to give these dotfiles a try, you should first fork this repository, review the code, and remove things you don’t want or need. Don’t blindly use my settings unless you know what that entails. Use at your own risk!
 
-```bash
+````bash
 sh -c "`curl -fsSL https://raw.githubusercontent.com/snics/dotfiles/master/install.sh`"
-```
+````
 
 ## Feedback
 
@@ -18,3 +18,17 @@ Suggestions/improvements
 | [![twitter/NicoSwiatecki](http://gravatar.com/avatar/23a38342df4d30085f1bbe71058cc89b?s=70)](http://twitter.com/NicoSwiatecki "Follow @NicoSwiatecki on Twitter") |
 |---|
 | [Nico Swiatecki](https://swiatecki.io/) |
+
+
+## Sensible macOS defaults
+
+When setting up a new Mac, you may want to set some sensible macOS defaults:
+
+```bash
+./MacOS/macos.sh
+```
+
+#### Sensible macOS dock defaults:
+```bash
+./MacOS/dock.sh
+```
