2dedb4a46b1c4d7159a85c31b34a615e67c66692
Nico Swiatecki <nico@devninjas.io>
2020-04-04 17:39:38 +0200

    Fix zsh plugin patch

--- zsh/install.sh
+++ zsh/install.sh
@@ -26,7 +26,7 @@ brew install fasd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 wget https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -O $HOME/.oh-my-zsh/custom/themes/bullet-train.zsh-theme
-cp -f $HOME/.dotfiles/zsh/plugins/*.zsh $HOME/.oh-my-zsh/custom/plugins
+cp -f $HOME/.dotfiles/zsh/plugins/ $HOME/.oh-my-zsh/custom/plugins
 
 cp -f $HOME/.dotfiles/zsh/.zshrc ~/.zshrc
 
