70777295b605c1cf24f0abde0d77ce1f9ea93bb9
Nico Swiatecki <nico@devninjas.io>
2020-06-10 19:42:32 +0200

    ✨ Add some good npm plugins

--- node/install.sh
+++ node/install.sh
@@ -20,5 +20,15 @@ brew install deno
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 echo ""
+echo "Install node CLI tools!"
+echo ""
+npm i -g npkill
+npm i -g npm-upgrade
+npm i -g npm-check
+npm i -g np
+npm i -g npm-name-cli
+
+
+echo ""
 echo "Install node Node.js and NVM done!"
 echo ""
