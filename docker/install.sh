f9a01ef03cdb33976a1e3c07efbcf0f801c45f20
Nico Swiatecki <nico@devninjas.io>
2023-08-23 13:52:38 +0200

    🔧 Add Docker and Kubernetes tools
    This commit adds several tools related to Docker and Kubernetes. It includes the installation of Docker, docker-compose, lazydocker, dive, skopeo, container-diff, lynis, sysdig, ctop, hadolint, clair, reg, docker-slim, minikube, kubernetes-cli (kubectl), kind (Kubernetes IN Docker), helm package manager for Kubernetes. Additionally it installs podman and podman-desktop for managing containers without a central daemon. Lima is installed as a lightweight virtualization tool to run Linux VMs. Trivy is added as a vulnerability scanner for containers. openshift-cli is included for managing OpenShift clusters. Kompose is installed to help users transition from 'docker-compose' to Kubernetes. Lens and kubenav are added as GUIs for managing Kubernetes clusters. Finally rancher-cli and rke are installed as Rancher tools.
    
    Co-authored-by: [Author Name] <[email protected]>

--- docker/install.sh
+++ docker/install.sh
@@ -17,39 +17,94 @@ brew upgrade
 
 brew tap "homebrew/cask"
 
-# Docker
+## Docker
+
+# Docker allows you to run applications in containers. It is an open platform for containerization.
 brew install --cask docker
+
+# docker-compose is a tool for defining and running multi-container Docker applications.
 brew install docker-compose
+
+# lazydocker is a simple terminal UI for Docker with keyboard shortcuts.
 brew install lazydocker
 
-# Kubernetes
+# dive is a tool to explore a Docker image, layer contents, and discover ways to reduce image size.
+brew install dive
+
+# Skopeo is a command-line utility that performs various operations on container images and image repositories. In English: Skopeo is a command line utility to work with container images and repositories.
+brew install skopeo
+
+# Container-diff is a tool to analyze differences between container images. In English: Container-diff is a tool for analyzing and comparing container images.
+brew install container-diff
+
+# Lynis is a security auditing tool for UNIX systems, like Linux, macOS, and others. In English: Lynis is a security and system auditing tool for Unix-based systems.
+brew install lynis
+
+# Sysdig is an open-source system monitoring and tracing tool.
+brew install sysdig
+
+# Ctop provides a concise real-time view of container metrics.
+brew install ctop
+
+# Hadolint is a linter for Dockerfiles to ensure best practices are followed.
+brew install hadolint
+
+# Clair is an open-source project for the static analysis of vulnerabilities in container images.
+brew install clair
+
+# Reg is a lightweight tool to interact with Docker registry.
+brew install reg
+
+# Docker-slim optimizes and reduces the size of Docker images without manual tuning.
+brew install docker-slim
+
+## Kubernetes
+
+# Minikube is a tool that allows running Kubernetes locally. In German: Minikube ist ein Werkzeug, das es ermöglicht, Kubernetes lokal auszuführen.
 brew install minikube
+
+# kubernetes-cli (kubectl) is a command-line tool to interact with Kubernetes clusters.
 brew install kubernetes-cli
+
+# kind stands for Kubernetes IN Docker. It runs local Kubernetes clusters using Docker container "nodes".
 brew install kind
 
-# Rancher tools
-brew install rancher-cli
-brew install rke
-
-# Helm
+# Helm is a package manager for Kubernetes.
 brew install helm
 
-# podman
+# podman is a tool to manage containers and images, similar to Docker, but without a central daemon.
 brew install podman
+
+# podman-desktop provides a GUI for the podman tool.
 brew install --cask podman-desktop
 
-# Lima
+# Lima is a lightweight virtualization tool that allows you to run Linux VMs with ease.
 brew install lima
 
-# trivy
+# Trivy is a vulnerability scanner for containers and other artifacts.
 brew install trivy
 
-# OpenShift
+brew install kompose
+
+# openshift-cli is a command-line tool for managing OpenShift, a Kubernetes distribution from Red Hat.
 brew install openshift-cli
 
-# Kuberntes GUIs
+# Kompose is a tool to help users familiar with 'docker-compose' transition to Kubernetes.
+brew install kompose
+
+# Kubernetes GUIs
+# Lens is a Kubernetes integrated development environment (IDE).
 brew install --cask lens
+
+# kubenav is a mobile, desktop, and web-based application to manage Kubernetes clusters.
 brew install --cask kubenav
+
+## Rancher tools
+# rancher-cli is a command-line tool for interacting with Rancher, a container management platform.
+brew install rancher-cli
+
+# rke (Rancher Kubernetes Engine) is a program to run Kubernetes clusters within containers.
+brew install rke
 
 # Remove outdated versions from the cellar.
 brew cleanup
