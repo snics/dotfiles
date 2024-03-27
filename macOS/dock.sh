49da6121cef884fb28ac51791e138a915be75f9b
Nico Swiatecki <nico@devninjas.io>
2024-03-27 18:09:13 +0100

    ✨🔧 Update dock configuration
    This commit updates the dock configuration by adding new applications and removing others. The changes include:
    
    - Added '/Applications/Arc.app' to the dock
    - Removed '/Applications/Cardhop.app' from the dock
    - Replaced '/Applications/Visual Studio Code.app' with '/Applications/Zed.app'
    
    These changes were made in 'macOS/dock.sh' and 'zsh/plugins/snics-settings/dock.zsh'.

--- macOS/dock.sh
+++ macOS/dock.sh
@@ -130,20 +130,20 @@ dockutil --no-restart --add '/Applications/Spotify.app'
 defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
 
 dockutil --no-restart --add '/Applications/Google Chrome.app'
+dockutil --no-restart --add '/Applications/Arc.app'
 
 # Add space to System configuration
 defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
 
 dockutil --no-restart --add "/Applications/Superhuman.app"
 dockutil --no-restart --add "/Applications/Fantastical.app"
-dockutil --no-restart --add "/Applications/Cardhop.app"
 dockutil --no-restart --add "/Applications/Todoist.app"
 dockutil --no-restart --add "/Applications/Franz.app"
 
 # Add space to System configuration
 defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
 
-dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Zed.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/GitKraken.app"
 
--- zsh/plugins/snics-settings/dock.zsh
+++ zsh/plugins/snics-settings/dock.zsh
@@ -2,13 +2,14 @@ mkdock () {
     echo "Make dock..."
 
     dockutil --no-restart --remove all
-    dockutil --no-restart  --add '/System/Applications/System Settings.app'
+    dockutil --no-restart --add '/System/Applications/System Settings.app'
     dockutil --no-restart --add '/Applications/Spotify.app'
 
     # Add space to System configuration
     defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
 
     dockutil --no-restart --add '/Applications/Google Chrome.app'
+    dockutil --no-restart --add '/Applications/Arc.app'
 
     # Add space to System configuration
     defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
@@ -16,13 +17,12 @@ mkdock () {
     dockutil --no-restart --add "/Applications/Superhuman.app"
     dockutil --no-restart --add "/Applications/Fantastical.app"
     dockutil --no-restart --add "/Applications/Todoist.app"
-    dockutil --no-restart --add "/Applications/Notion.app"
     dockutil --no-restart --add "/Applications/Franz.app"
 
     # Add space to System configuration
     defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type='spacer-tile';}'
 
-    dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+    dockutil --no-restart --add "/Applications/Zed.app"
     dockutil --no-restart --add "/Applications/iTerm.app"
     dockutil --no-restart --add "/Applications/GitKraken.app"
 
