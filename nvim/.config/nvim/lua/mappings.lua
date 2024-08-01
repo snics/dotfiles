a69ff8bdcb81f9d8b6e5beb7de98d2c5bc59bf2e
Nico Swiatecki <nico@devninjas.io>
2024-08-01 21:34:37 +0200

    ✨🔧 Refactor code structure and mappings
    - Reorganized the code structure in `lazy.lua` for better readability.
    - Updated key mappings in `mappings.lua` to improve user experience.
    - Added a new file `completions.lua` to handle plugin completions.
    - Modified the initialization of plugins in `init.lua` for better organization.
    
    This commit improves the code structure and enhances key mappings, making it easier to navigate and use the application. It also introduces a new file to handle plugin completions and updates the initialization process of plugins for better organization.

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -4,7 +4,15 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- General Keymaps
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "jk to escape" })
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "Save" })
+map("n", "<leader>q", "<cmd> q <cr>", { desc = "Quit" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- LSP Keymaps
+map("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+map("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+map("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go to References" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
