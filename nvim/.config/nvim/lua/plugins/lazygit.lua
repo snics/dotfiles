8454af904ef345fa9d73950d9dbed04002d84385
Nico Swiatecki <nico@devninjas.io>
2024-09-04 17:05:10 +0200

    ✨🔧 Added gitsigns.nvim and lazygit.nvim plugins
    The commit adds the gitsigns.nvim and lazygit.nvim plugins to the Neovim configuration. The gitsigns plugin provides Git integration with features like navigating hunks, staging/resetting hunks, previewing hunks, blaming lines, and more. The lazygit plugin allows opening a lazy git interface with various commands for managing Git repositories.

---
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,16 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>Go", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+  },
+}
