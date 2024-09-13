c616434c045ba02e219411af807ad2db30262e6d
Nico Swiatecki <nico@devninjas.io>
2024-09-13 18:00:37 +0200

    ✨🔧 Refactor window padding configuration
    The code changes refactor the window padding configuration in both Neovim and Wezterm. In Neovim, the `wezterm.nvim` plugin is removed and replaced with custom Lua code to set the window padding on VimEnter and VimLeavePre events. In Wezterm, a new event handler is added to update the right status of the terminal window based on whether Neovim is running or not. The default window padding values are also adjusted.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,12 +13,32 @@ config.window_decorations = "RESIZE"
 config.default_cursor_style = 'SteadyUnderline'
 
 -- Default window padding
-config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 0,
-  bottom = 10,
+local default_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
 }
+config.window_padding = default_padding
+
+
+wezterm.on("update-right-status", function(window, pane)
+    local process_name = pane:get_foreground_process_name()
+    if process_name and process_name:find("nvim") then
+        window:set_config_overrides({
+            window_padding = {
+                left = 0, -- Custom Padding für nvim
+                right = 0,
+                top = 0,
+                bottom = 0,
+            }
+        })
+    else
+        window:set_config_overrides({
+            window_padding = default_padding
+        })
+    end
+end)
 
 
 -- Set Theme settings
