47921e035116f72322b4a6edda70e6d91f9c5d99
Nico Swiatecki <nico@devninjas.io>
2026-02-08 03:11:09 +0100

    ✨ feat(autopairs): add fast_wrap, map_c_w, snacks filetype exclusion

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,29 +1,38 @@
 return {
-	"windwp/nvim-autopairs",
-	event = { "InsertEnter" },
-	dependencies = {
-		"hrsh7th/nvim-cmp",
-	},
-	config = function()
-		-- import nvim-autopairs
-		local autopairs = require("nvim-autopairs")
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("nvim-autopairs").setup({
+            check_ts = true,
+            ts_config = {
+                lua = { "string", "source", "string_content" },
+                javascript = { "string", "template_string" },
+            },
+            disable_filetype = { "TelescopePrompt", "snacks_picker_input" },
+            disable_in_macro = true,
+            disable_in_replace_mode = true,
+            enable_moveright = true,
+            enable_check_bracket_line = true,
+            map_cr = true,
+            map_bs = true,
+            map_c_w = true, -- Ctrl+W in insert mode also removes the pair: (|) → Ctrl+W → |
+            fast_wrap = {
+                map = "<M-e>", -- Alt+E to wrap: cursor before hello → Alt+E → select → (hello)
+                chars = { "{", "[", "(", '"', "'" },
+                end_key = "$",
+                before_key = "h",
+                after_key = "l",
+                keys = "qwertyuiopzxcvbnmasdfghjkl",
+                highlight = "Search",
+                highlight_grey = "Comment",
+            },
+        })
 
-		-- configure autopairs
-		autopairs.setup({
-			check_ts = true, -- enable treesitter
-			ts_config = {
-				lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-				javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-			},
-		})
-
-		-- import nvim-autopairs completion functionality
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-		-- import nvim-cmp plugin (completions plugin)
-		local cmp = require("cmp")
-
-		-- make autopairs and completion work together
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end,
+        -- nvim-cmp integration: auto-insert () after completing a function/method
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
 }
