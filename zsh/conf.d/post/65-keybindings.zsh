a9b7c68285c5dffbff16d305bf03e507da0e272f
Nico Swiatecki <nico@devninjas.io>
2026-02-10 02:21:51 +0100

    🐛 fix(keybindings): bind macOS Alt+C (ç) to fzf cd widget
    On macOS, Option+C produces the literal character 'ç' instead of
    the \ec escape sequence that fzf expects. Bind ç directly to
    fzf-cd-widget so Alt+C directory switching works.

--- zsh/conf.d/post/65-keybindings.zsh
+++ zsh/conf.d/post/65-keybindings.zsh
@@ -1,2 +1,6 @@
 # Emacs keybindings
 bindkey -e
+
+# macOS Alt+C fix: Option+C produces 'ç' instead of \ec escape sequence.
+# Bind the literal character to fzf's cd widget so Alt+C works as expected.
+bindkey 'ç' fzf-cd-widget
