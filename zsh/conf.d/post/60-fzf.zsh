6575d24e2345a7dfc86822cc9dbb50b0ce175820
Nico Swiatecki <nico@devninjas.io>
2026-02-10 02:25:04 +0100

    ✨ feat(fzf): add eza tree preview to Alt+C directory switcher
    Override zimfw/fzf default ls -1F preview with eza tree for the
    Alt+C cd widget, matching the Ctrl+T directory preview style.

--- zsh/conf.d/post/60-fzf.zsh
+++ zsh/conf.d/post/60-fzf.zsh
@@ -1,2 +1,3 @@
-# Override zimfw/fzf preview: bat without header, eza tree for directories
+# Override zimfw/fzf previews: eza tree for directories, bat for files
 export FZF_CTRL_T_OPTS="--bind ctrl-/:toggle-preview --preview 'if [[ -d {} ]]; then eza --tree --level 3 --icons=auto --color=always -a {}; else bat --style=numbers --color=always --line-range :500 {}; fi'"
+export FZF_ALT_C_OPTS="--bind ctrl-/:toggle-preview --preview 'eza --tree --level 3 --icons=auto --color=always -a {}'"
